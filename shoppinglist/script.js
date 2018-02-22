4cb88f77a71ac21d78d26563f1d103740f9bdcb6
Kevin Murphy <midimurphdesigns@gmail.com>
2018-02-22 15:06:00 -0700

    finished shopping list app!

--- shoppinglist/script.js
+++ shoppinglist/script.js
@@ -1,7 +1,10 @@
-function addItem() {
-  $('#js-shopping-list-form').submit(event => {
+$(function() {
+
+  //adding an event listener to the form, to add an item to the shopping list
+
+  $('#js-shopping-list-form').submit(function(event) {
     event.preventDefault();
-    const inputVal = $('input').val(); 
+    const inputVal = $('.js-shopping-list-entry').val(); 
     $('.shopping-list').append(` 
       <li>
         <span class="shopping-item">${inputVal}</span>
@@ -15,7 +18,21 @@ function addItem() {
         </div>
       </li>  
     `);
+    $('input').val('');
+    // document.getElementsByClassName('js-shopping-list-entry')[0].value='';
   });
-}
 
-$(addItem);
+  //adding a delegated event listener to toggle check class and check off an item on shopping list
+
+  $('.shopping-list').on('click', '.shopping-item-toggle', function(event) {
+    $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+    console.log('toggle', $(this).closest('li'));
+  });
+
+  //adding a delegated event listener to remove an item from the shooping list
+
+  $('.shopping-list').on('click', '.shopping-item-delete', function(event) {
+    $(this).closest('li').remove();
+    console.log('delete', $(this).closest('li'));
+  });
+});
