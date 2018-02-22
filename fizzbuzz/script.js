ec26e754eedc19d067a87fee630723868b4f5aa7
George Colon <geocolon@gmail.com>
2018-02-22 12:23:00 -0800

    work on Fizz Buzz

--- fizzbuzz/script.js
+++ fizzbuzz/script.js
@@ -1,1 +1,34 @@
-// your code here
+function fizzbuzz(num){
+    if (num % 5 === 0){
+        return 'buzz';
+    }
+    else if (num % 3 === 0){
+        return 'fizz';
+    }
+    else if (num % 15 === 0){
+        return 'fizz buzz';
+    }
+    else {
+        return num;
+    }
+}
+
+function createEle(){
+    const elem = document.createElement('div');
+    if (typeof ){
+
+    }
+}
+
+function blindEventListeners(){
+    $('#number-chooser').submit((event) => {
+      event.preventDefault();
+      const numberInput = $(event.target).find('#number-choice');
+      const countTo = parseInt(numberInput.val(), 10);
+      numberInput.val('');
+      const data = fizzbuzz(countTo);
+      $('.js-results').html(elements);
+    });
+}
+
+$(blindEventListeners);
